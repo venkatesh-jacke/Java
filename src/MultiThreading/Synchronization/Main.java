058e8f8d18bc9303418b63ce02a5885a72198d5c
venkatesh-jacke <venkateshjacke@gmail.com>
2024-03-21 14:39:44 +0530

    MultiThreading-synchronization

---
+++ src/MultiThreading/Synchronization/Main.java
@@ -0,0 +1,56 @@
+package MultiThreading.Synchronization;
+
+public class Main {
+    public static void main(String[] args) {
+        CallMe callMe= new CallMe();
+        Caller o1= new Caller(callMe,"venkat");
+        Caller o2= new Caller(callMe,"Jacke");
+        Caller o3= new Caller(callMe,"Android");
+    }
+    static  class CallMe{
+        synchronized void display(String msg){
+            System.out.print("["+msg);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.print("]");
+        }
+    }
+    static  class Caller implements Runnable{
+        CallMe obj;
+        String msg;
+        Thread t;
+
+        public Caller(CallMe obj, String msg) {
+            this.obj = obj;
+            this.msg = msg;
+            t=new Thread(this);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            obj.display(msg);
+        }
+    }
+}
+
+/*
+Without Synchronization
+void display(String msg)
+[venkat[Android[Jacke]]]
+
+With Synchronization
+synchronized void display(String msg)
+[venkat][Android][Jacke]
+
+if you dont need to alter the original method of a class you can place the critical section of a code inside
+Synchronized block
+synchronized(obj){
+//CODE
+}
+*/
